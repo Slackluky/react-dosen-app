0db3a55863f6aa02d3e34a9ccf89fd75d19d00b0
Slackluky <unluckly17@gmail.com>
2023-05-15 13:19:37 +0700

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ function App() {
             })}
           </tbody>
         </table>
-        <div>
+        <div className="flex justify-end">
           <button
             className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
             onClick={() => setOutput(result)}
