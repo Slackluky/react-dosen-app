b585d02ac6ca83f934d2da97fef8ea1456dc4b9d
Slackluky <unluckly17@gmail.com>
2023-06-03 19:46:11 +0700

    update to typescript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,41 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+    env: { browser: true, es2020: true, node: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:import/recommended'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            tsx: true
+        }
+    },
+    settings: { react: { version: '18.2' },'import/resolver': {
+        'node': {
+            'extensions': ['.js', '.jsx', '.ts', '.tsx']
+        }
+    } },
+    plugins: ['react-refresh', 'react', '@typescript-eslint', 'import'],
+    rules: {
+        'react-refresh/only-export-components': 'warn',
+        'react/jsx-indent': ['error', 4],
+        'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+        '@typescript-eslint/indent': ['error', 4, { ignoredNodes: ['JSXElement *', 'JSXElement'] }],
+        indent: 'off',
+        'react/jsx-uses-react': 'error',
+        'linebreak-style': ['error', 'windows'],
+        'no-use-before-define': 'off',
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+
+        // override configuration set by extending "eslint:recommended"
+        'no-empty': 'warn',
+        'no-cond-assign': ['error', 'always'],
+
+        // disable rules from base configurations
+        'for-direction': 'off',
+        'comma-dangle': ['error', 'never']
+    }
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,117 @@
+/* eslint-disable react/jsx-no-useless-fragment */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/no-unstable-nested-components */
 import React, { useState } from 'react';
 import { Icon } from '@iconify/react';
 
 function App() {
-  const mahasiswa = Array.from({ length: 10 }).map((it, index) => {
-    return { id: `mahasiswa${index + 1}`, name: `Mahasiswa ${index + 1}` };
-  });
-  const penelitian = Array.from({ length: 4 }).map((it, index) => {
-    return {
-      id: `aspek_penelitian_${index + 1}`,
-      name: `Aspek Penelitian ${index + 1}`,
-    };
-  });
-  const [result, setResult] = useState(
-    penelitian.reduce(
-      (research, { id }) => (
-        (research[id] = mahasiswa.reduce(
-          (acc, { id: mIdx }) => ((acc[mIdx] = 0), acc),
-          {}
-        )),
-        research
-      ),
-      {}
-    )
-  );
-
-  const handleChangeValue = (e, pId, mId) => {
-    e.preventDefault();
-    setResult({
-      ...result,
-      [pId]: {
-        ...result[pId],
-        [mId]: parseInt(e.target.value),
-      },
-    });
-  };
-
-  const [output, setOutput] : [object, Function] = useState({});
-
-  function Output() {
-    if (Object.keys(output).length <= 0) {
-      return <></>;
-    }
-    return <code>{JSON.stringify(output, null, 4)}</code>;
-  }
-  return (
-    <>
-      <div className="w-full container mx-auto flex flex-col gap-y-3 my-4">
-        <table className="table-auto w-full text-sm text-center text-gray-500">
-          <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
-            <tr>
-              <th className="px-6 py-3"></th>
-              {penelitian.map(({ id: pId, name: pName }) => {
-                return (
-                  <th key={pId} className="px-6 py-3">
-                    {pName}
-                  </th>
+    const mahasiswa = Array.from({ length: 10 }).map((it, index) => ({ id: `mahasiswa${index + 1}`, name: `Mahasiswa ${index + 1}` }));
+    const penelitian = Array.from({ length: 4 }).map((it, index) => ({
+        id: `aspek_penelitian_${index + 1}`,
+        name: `Aspek Penelitian ${index + 1}`
+    }));
+    const [result, setResult] = useState(
+        penelitian.reduce(
+            (research, { id }) => {
+                const r = research;
+                const a = (
+                    (r[id] = mahasiswa.reduce(
+                        (acc, { id: mIdx }) => { const b = acc; b[mIdx] = 0; return b; },
+                        {}
+                    )),
+                    r
                 );
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {mahasiswa.map(({ id: mId, name: mahas }) => {
-              return (
-                <tr key={mId} className="bg-white border-b">
-                  <th className="px-6 h-full font-medium text-gray-900">
-                    <span className='h-full flex items-center gap-x-3 justify-center'>
-                    <Icon icon="material-symbols:account-circle" className='text-[24px] text-gray-500'></Icon>
-                    {mahas}
-                    </span>
-                    
-                  </th>
-                  {penelitian.map(({ id: pId }) => {
-                    return (
-                      <td key={pId} className="px-6 py-4">
-                        <select
-                          title="change student value"
-                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                          value={result[pId][mId]}
-                          onChange={(e) => handleChangeValue(e, pId, mId)}
-                        >
-                          {Array.from({ length: 11 }).map((it, index) => (
-                            <option key={index}>{index}</option>
-                          ))}
-                        </select>
-                      </td>
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <div className="flex justify-end">
-          <button
-            className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
-            onClick={() => setOutput(result)}
-          >
-            Simpan
-          </button>
-          <button
-            className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
-            onClick={() => setOutput('')}
-          >
-            Reset
-          </button>
+                return a;
+            },
+            {}
+        )
+    );
+
+    const handleChangeValue = (e, pId, mId) => {
+        e.preventDefault();
+        setResult({
+            ...result,
+            [pId]: {
+                ...result[pId],
+                [mId]: parseInt(e.target.value, 10)
+            }
+        });
+    };
+
+    const [output, setOutput] : [object, Function] = useState({});
+
+    function Output() {
+        if (Object.keys(output).length <= 0) {
+            return <></>;
+        }
+        return <code>{JSON.stringify(output, null, 4)}</code>;
+    }
+
+    return (
+        <div className="w-full container mx-auto flex flex-col gap-y-3 my-4">
+            <table className="table-auto w-full text-sm text-center text-gray-500">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
+                    <tr>
+                        <th className="px-6 py-3" aria-label="table head" />
+                        {penelitian.map(({ id: pId, name: pName }) => (
+                            <th key={pId} className="px-6 py-3">
+                                {pName}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {mahasiswa.map(({ id: mId, name: mahas }) => (
+                        <tr key={mId} className="bg-white border-b">
+                            <th className="px-6 h-full font-medium text-gray-900">
+                                <span className="h-full flex items-center gap-x-3 justify-center">
+                                    <Icon icon="material-symbols:account-circle" className="text-[24px] text-gray-500" />
+                                    {mahas}
+                                </span>
+
+                            </th>
+                            {penelitian.map(({ id: pId }) => (
+                                <td key={pId} className="px-6 py-4">
+                                    <select
+                                      title="change student value"
+                                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                      value={result[pId][mId]}
+                                      onChange={(e) => handleChangeValue(e, pId, mId)}
+                                    >
+                                        {Array.from({ length: 11 }).map((it, index) => (
+                                            <option key={`${index * 3}sIndex`}>{index}</option>
+                                        ))}
+                                    </select>
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div className="flex justify-end">
+                <button
+                  type="button"
+                  className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
+                  onClick={() => setOutput(result)}
+                >
+                    Simpan
+                </button>
+                <button
+                  type="button"
+                  className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
+                  onClick={() => setOutput('')}
+                >
+                    Reset
+                </button>
+            </div>
+            <div className="bg-black w-full p-5 text-white whitespace-pre">
+                Output:
+                {' '}
+                <br />
+                <Output />
+            </div>
         </div>
-        <div className="bg-black w-full p-5 text-white whitespace-pre">
-          Output: <br />
-          <Output />
-        </div>
-      </div>
-    </>
-  );
+    );
 }
 
 export default App;
